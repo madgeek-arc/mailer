84304c23d6d6f5aa9661f57c8661b191af643ad4
Konstantinos Spyrou <spyroukon@gmail.com>
2025-01-09 15:23:12 +0200

    build(spring/java): bump spring-boot and java versions

--- mailer-service/src/main/java/gr/athenarc/messaging/mailer/MailerApplication.java
+++ mailer-service/src/main/java/gr/athenarc/messaging/mailer/MailerApplication.java
@@ -1,14 +1,13 @@
 package gr.athenarc.messaging.mailer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MailerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MailerApplication.class, args);
+    }
 
 }
